338ad78c31a0ea16bb11b52c39f4528b14cfd56d
Daniel-PeGa <danielperezgarnic@gmail.com>
2022-11-06 10:51:11 -0600

    some progress on the server

--- server/models/index.js
+++ server/models/index.js
@@ -1,39 +1,6 @@
-const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const path = require('path');
+const Church = require('./Church');
+const Group = require('./Group');
+const SmallGroup = require('./SmallGroup');
+const User = require('./User');
 
-const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
-
-const PORT = process.env.PORT || 3001;
-const app = express();
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-
-
-const startApolloServer = async (typeDefs, resolvers) => {
-  await server.start();
-  server.applyMiddleware({ app });
-  
-  db.once('open', () => {
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-    })
-  })
-  };
-  
-  startApolloServer(typeDefs, resolvers);
+module.exports = { Church, Group, SmallGroup, User };
