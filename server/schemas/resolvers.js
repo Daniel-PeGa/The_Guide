49a22a246592b938a11f51210a96dbb789b1517f
Daniel-PeGa <danielperezgarnic@gmail.com>
2022-11-06 17:01:53 -0600

    individual search in resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,12 +2,17 @@ const { Group, User, Church, SmallGroup } = require('../models');
 
 const resolvers = {
     Query: {
+
         groups: async () => {
             return await Group.find({}).populate({
                 path: 'users',
                 populate: 'name'
             });
         },
+        group: async (parent, args) => {
+            return await Group.findById(args.id).populate('users');
+        },
+
         users: async () => {
             return await User.find({}).populate({
                 path: 'churches',
@@ -20,18 +25,31 @@ const resolvers = {
                 populate: 'name'
             });
         },
+        user: async (parent, args) => {
+            return await User.findById(args.id).populate('churches', 'smallGroups', 'groups');
+        },
+
         churches: async () => {
             return await Church.find({}).populate({
                 path: 'users',
                 populate: 'name'
-            };
+            });
         },
+        church: async (parent, args) => {
+            return await Church.findById(args.id).populate('users');
+        },
+
         smallGroups: async () => {
             return await SmallGroup.find({}).populate({
                 path: 'users',
                 populate: 'name'
-            });
+            })
+        },
+        smallGroup: async (paarent, args) => {
+            return await SmallGroup.findById(args.id).populate('users');
         }
+
+
     }
 };
  
