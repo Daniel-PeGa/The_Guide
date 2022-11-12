b5a6cf7c50d238766b81d73ed7fa907a79799c5e
Daniel Perez Garnica <danielperezgarnic@gmail.com>
2022-11-11 18:19:45 -0600

    mutations to add the user

---
+++ client/src/components/UserForm/index.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../../utils/mutations';
+
+const UserForm = () => {
+    const [username, setUsername] = useState('');
+
+    const [addUser, { error }] = useMutation(ADD_USER);
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const { data}  = await addUser({
+                variables: { username },
+            });
+
+            window.location.reload();
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    return (
+
+// whats to be returned to add the user
+
+    );
+};
+
+export default UserForm;
