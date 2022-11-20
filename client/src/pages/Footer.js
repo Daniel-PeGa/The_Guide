f97f08f80b95a5a8afb4b58a0536d03c8a1545f9
Daniel-PeGa <danielperezgarnic@gmail.com>
2022-11-20 14:30:28 -0600

    stripe installed in the server package json still not workin tho

--- client/src/pages/Footer.js
+++ client/src/pages/Footer.js
@@ -1,19 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Button = styled.button`
+padding: 5px 15px;
+border-radius: 5px;
+outline: 0;
+text-transform: uppercase;
+margin: 5px;
+cursor: pointer;
+box-shadow: 0px 2px 2px lightgray;
+`
 
 function Footer() {
     return (
         <footer className="footer">
         <div className="content has-text-centered">
           <p>
-            <button>Request</button> 
+            <Button disabled>Request</Button> 
             <p>
               Romans 5:8
             </p>
-            <button>Donate</button> 
+            <Button onclick={donateBtn}>Donate</Button> 
           </p>
         </div>
       </footer>  
     );
+}
+
+function donateBtn(){
 }
 
 export default Footer;
